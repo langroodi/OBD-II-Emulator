ec8eacc05189341ed40c3edc334cc10808279305
Mohandes Zalatan <mohandes.zalatan@gmail.com>
2022-03-12 11:31:30 +0100

    :beer: Implement the ODB emulator sample

--- samples/obd_emulator_sample.cpp
+++ samples/obd_emulator_sample.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
 #include "./current_data_obd_service.h"
+#include "../include/obdemulator/obd_emulator.h"
+#include "../include/obdemulator/serial_communication.h"
 
 int main()
 {
-    std::cout << "Sample has been started. Press Ctrl+C to quit...\n";
+    const std::string cSerialPort{"/dev/ttyUSB0"};
+    const speed_t cBaudrate{115200};
+
+    ObdEmulator::SerialCommunication serialCommunication(cSerialPort, cBaudrate);
+    ObdEmulator::CurrentDataObdService currentDataObdService;
+    ObdEmulator::ObdEmulator obdEmulator(&serialCommunication, {&currentDataObdService});
+
+    bool _succeed{obdEmulator.TryStart()};
+    if (_succeed)
+    {
+        std::cout << "Sample has been started successfully. Press any key to quit...\n";
+    }
+    else
+    {
+        std::cout << "Sample has been failed to start. Press any key to quit...\n";
+    }
+
+    // Avoid the console to be closed
+    std::getchar();
+
+    if (_succeed)
+    {
+        _succeed = obdEmulator.TryStop();
+
+        if (_succeed)
+        {
+            std::cout << "Sample has been stopped successfully.\n";
+        }
+        else
+        {
+            std::cout << "Sample has been failed to gracefully stop.\n";
+        }
+    }
 
     return 0;
 }
